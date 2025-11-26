dabfe59a34beffaea64f0ed16ab3867c5f714595
JauniKappauni <social.jauni@gmail.com>
2025-11-26 20:05:08 +0100

    feat: Add second square

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include "C:\Users\grimm\Desktop\raylib-5.5_win64_mingw-w64\raylib-5.5_win64_mingw-w64\include\raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
+const int screenWidth = 800;
+const int screenHeight = 400;
+
+int generatePosX()
+{
+    return rand() % screenWidth;
+}
+
+int generatePosY()
+{
+    return rand() % screenHeight;
+}
 
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 400;
-
+    srand(time(0));
     InitWindow(screenWidth, screenHeight, "SquareCollect");
     SetTargetFPS(60);
 
     float posX = 20;
     float posY = 20;
 
+    int squarePosX = generatePosX();
+    int squarePosY = generatePosY();
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
         ClearBackground(WHITE);
         DrawText("SquareCollect", 350, 200, 20, GRAY);
         float dt = GetFrameTime();
@@ -27,7 +44,9 @@ int main()
             posY -= speed * dt;
         if (IsKeyDown(KEY_S))
             posY += speed * dt;
+
         DrawRectangle(posX, posY, 20, 20, GRAY);
+        DrawRectangle(squarePosX, squarePosY, 20, 20, GRAY);
         EndDrawing();
     }
     CloseWindow();
